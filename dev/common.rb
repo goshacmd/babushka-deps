fc8390c5c635b74854ed05b52ce04a83e8e68d48
Gosha Arinich <me@goshakkk.name>
2012-06-13 16:19:00 +0300

    add hub

--- dev/common.rb
+++ dev/common.rb
@@ -3,11 +3,13 @@ dep 'wget.managed'
 dep 'tree.managed'
 dep 'ctags.managed'
 dep 'ack.managed'
+dep 'hub.managed'
 
 dep 'common-dev' do
   requires 'tmux.managed',
     'wget.managed',
     'tree.managed',
     'ctags.managed',
-    'ack.managed'
+    'ack.managed',
+    'hub.managed'
 end
