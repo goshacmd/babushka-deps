b5aca4d1c21fad3da67ae2b1ec674b278f0f8ed8
Gosha Arinich <me@goshakkk.name>
2012-06-18 17:43:02 +0300

    add manservant

--- dev/common.rb
+++ dev/common.rb
@@ -5,11 +5,19 @@ dep 'ctags.managed'
 dep 'ack.managed'
 dep 'hub.managed'
 
+dep 'manservant' do
+  requires 'pow.managed'
+
+  met? { (ENV['HOME'] / '.pow/man').dir? }
+  meet { git "git://github.com/jimeh/manservant.git", :to => ENV['HOME'] / '.pow/man' }
+end
+
 dep 'common-dev' do
   requires 'tmux.managed',
     'wget.managed',
     'tree.managed',
     'ctags.managed',
     'ack.managed',
-    'hub.managed'
+    'hub.managed',
+    'manservant'
 end
