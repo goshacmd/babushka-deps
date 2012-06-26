00462f1fccc7c73fff9660d70c17f777d0a4e919
Gosha Arinich <me@goshakkk.name>
2012-06-26 15:10:14 +0300

    cljr

--- dev/clojure.rb
+++ dev/clojure.rb
@@ -1,5 +1,13 @@
-dep 'clojure.managed'
+dep 'clojure.managed' do
+  provides 'clj'
+end
+
+dep 'cljr' do
+  met? { which 'cljr' }
+  meet { shell 'wget http://incanter.org/downloads/cljr-installer.jar && java -jar cljr-installer.jar' }
+end
 
 dep 'clojure-dev' do
-  requires 'clojure.managed'
+  requires 'clojure.managed',
+    'cljr'
 end
