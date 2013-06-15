9c7fb8c8a1370806f33ec002ef5ed3d2a3c77070
Gosha Arinich <me@goshakkk.name>
2013-06-15 17:43:47 +0300

    remove cljr

--- dev/clojure.rb
+++ dev/clojure.rb
@@ -2,12 +2,6 @@ dep 'clojure.managed' do
   provides 'clj'
 end
 
-dep 'cljr' do
-  met? { which 'cljr' }
-  meet { shell 'wget http://incanter.org/downloads/cljr-installer.jar && java -jar cljr-installer.jar' }
-end
-
 dep 'clojure-dev' do
-  requires 'clojure.managed',
-    'cljr'
+  requires 'clojure.managed'
 end
